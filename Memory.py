73307623f4ff584359170ff74bdc678053ba4971
phaedraa <phaedra.a.r@gmail.com>
2015-07-17 09:33:43 -0700

    Refining Memory

--- Memory.py
+++ Memory.py
@@ -23,12 +23,13 @@ class Memory:
             raise KeyError("num_cards must be an even integer")
         if num_cards < 8 or num_cards > 20:
             raise KeyError("num_cards must be between [8, 20]")
-        self.deck_size = num_cards # deck_size must be an even integer in range [8, 20]
+        self.deck_size = num_cards # deck_size must be an even integer, [8, 20]
         self.BOARD_WIDTH = 50 * self.deck_size
         self.BOARD_HEIGHT = 100
         
         # create frame and add a button and labels
-        self.frame = simplegui.create_frame("Memory", self.BOARD_WIDTH, self.BOARD_HEIGHT)
+        self.frame = simplegui.create_frame("Memory", self.BOARD_WIDTH, 
+            self.BOARD_HEIGHT)
         self.frame.add_button("Reset", self.new_game)
         self.label = self.frame.add_label("Turns = 0")
     
@@ -43,12 +44,14 @@ class Memory:
     def new_game(self):
         self.state = 0
         self.turns = 0
+        self.card_1_idx = None
+        self.card_2_idx = None
         self.deck = []
         for card in range(self.deck_size / 2):
             self.deck.append(card)
             self.deck.append(card)
         random.shuffle(self.deck) 
-        self.exposed = [False for i in range(self.deck_size)]
+        self.exposed = [False for card in range(self.deck_size)]
 
     def expose(self):
         if self.mouse_pos[1] >= 0 and self.mouse_pos[1] <= self.BOARD_HEIGHT:
@@ -57,9 +60,10 @@ class Memory:
             x_pos_2 = increment
             card = 0
             found = False
-            while x_pos_2 <= self.BOARD_WIDTH and card < self.deck_size and not found:
+            while x_pos_2 <= self.BOARD_WIDTH and not found:
                 if self.exposed[card] != True:
-                    if self.mouse_pos[0] >= x_pos_1 and self.mouse_pos[0] <= x_pos_2:
+                    if self.mouse_pos[0] >= x_pos_1 and \
+                    self.mouse_pos[0] <= x_pos_2:
                         self.exposed[card] = True
                         if self.card_1_idx == None:
                             self.card_1_idx = card
@@ -83,11 +87,9 @@ class Memory:
             self.card_2_idx = None
             self.expose()
             self.turns += 1
-            
         elif self.state == 1:
             self.state = 2
             self.expose()
-            self.turns += 1
         else:
             self.state = 1
             self.expose()
@@ -98,12 +100,18 @@ class Memory:
         x_pos_2 = self.BOARD_WIDTH / self.deck_size
         for idx, card in enumerate(self.deck):
             if self.exposed[idx] == True:
-                canvas.draw_polygon([(x_pos_2,100), (x_pos_1,100), (x_pos_1,0), (x_pos_2,0)], 3, 'Black', 'Red')
-                canvas.draw_text(str(card), (x_pos_1 + (self.BOARD_WIDTH / self.deck_size) / 5, 0.7 * self.BOARD_HEIGHT), 0.6 * self.BOARD_HEIGHT, 'Black')
+                canvas.draw_polygon([
+                    (x_pos_2, self.BOARD_HEIGHT), (x_pos_1, self.BOARD_HEIGHT), 
+                    (x_pos_1,0), (x_pos_2,0)], 3, 'Black', 'Red')
+                canvas.draw_text(str(card), 
+                    (x_pos_1 + (self.BOARD_WIDTH / self.deck_size) / 5, 
+                    0.7 * self.BOARD_HEIGHT), 0.6 * self.BOARD_HEIGHT, 'Black')
             else:
-                canvas.draw_polygon([(x_pos_2,100), (x_pos_1,100), (x_pos_1,0), (x_pos_2,0)], 3, 'Black', 'Green')
+                canvas.draw_polygon([
+                    (x_pos_2, self.BOARD_HEIGHT), (x_pos_1, self.BOARD_HEIGHT), 
+                    (x_pos_1,0), (x_pos_2,0)], 3, 'Black', 'Green')
             x_pos_1 += self.BOARD_WIDTH / self.deck_size
             x_pos_2 += self.BOARD_WIDTH / self.deck_size
-        self.label.set_text("self.turns = " + str(self.turns))  
+        self.label.set_text("Total turns: " + str(self.turns))  
 
 main()
